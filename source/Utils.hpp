08f31cec1203ab4f33a74c62ca78c67d62ffc185
Domanat <maxim.tomilko@yandex.ru>
2022-03-04 21:19:57 +0300

    Add ResourceManager

--- source/Utils.hpp
+++ source/Utils.hpp
@@ -4,12 +4,15 @@
 #include <Windows.h>
 #include <string>
 
-inline std::string GetWorkingDirectory()
+namespace Utils
 {
-    CHAR buffer[MAX_PATH] = { 0 };
-    GetModuleFileNameA(NULL, buffer, MAX_PATH);
-    std::string::size_type pos = std::string(buffer).find_last_of("\\/");
-    return std::string(buffer).substr(0, pos);
+    inline std::string GetWorkingDirectory()
+    {
+        CHAR buffer[MAX_PATH] = { 0 };
+        GetModuleFileNameA(NULL, buffer, MAX_PATH);
+        std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+        return std::string(buffer).substr(0, pos);
+    }
 }
 
 #endif
